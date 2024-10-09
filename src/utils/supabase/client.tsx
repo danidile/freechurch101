e94ca28612865a087a06288d9f95f2cffdcfd96d
Daniele Di Lecce <alessialauriero@Dani-Mac.local>
2024-10-09 12:44:38 +0200

    added SUpabase sectrets directly to make check if this is the problem.

--- src/utils/supabase/client.tsx
+++ src/utils/supabase/client.tsx
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+     "https://kadorwmjhklzakafowpu.supabase.co",
+   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthZG9yd21qaGtsemFrYWZvd3B1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc2OTAzODYsImV4cCI6MjA0MzI2NjM4Nn0.HATaPSw8lCH1I1ETvbK-omIYeW1tzSHOEaAg821PygA"
   )
 }
