425c273da42150f150cae3bb0d97bd3da2ca2eae
Daniele Di Lecce <alessialauriero@Dani-Mac.local>
2024-10-17 18:00:55 +0200

    commit after major updates to user dashboard

--- src/app/protected/dashboard/dashboard-components/sidebar.tsx
+++ src/app/protected/dashboard/dashboard-components/sidebar.tsx
@@ -6,11 +6,11 @@ export default function Sidebar() {
  return (
     <div className="dashboard-sidebar-container">
         <ul className="dashboard-ul">
-            <li className="dashboard-li"> <Image className="dashboard-icon" src="/images/dashboard/dashboard.png"/> Dashboard</li>
-            <li className="dashboard-li"><Image className="dashboard-icon" src="/images/dashboard/song-lyrics.png"/> Canzoni</li>
-            <li className="dashboard-li"><Image className="dashboard-icon" src="/images/dashboard/church.png"/> La mia chiesa</li>
-            <li className="dashboard-li"> <Image className="dashboard-icon" src="/images/dashboard/settings.png"/>Impostazioni</li>
-            <li className="dashboard-li"> <Image className="dashboard-icon" src="/images/dashboard/user.png"/>Il mio Account</li>
+            <li className="dashboard-li"> <Image className="dashboard-icon" src="/images/dashboard/dashboard.png" alt="" /> Dashboard</li>
+            <li className="dashboard-li"><Image className="dashboard-icon" src="/images/dashboard/song-lyrics.png" alt="" /> Canzoni</li>
+            <li className="dashboard-li"><Image className="dashboard-icon" src="/images/dashboard/church.png" alt="" /> La mia chiesa</li>
+            <li className="dashboard-li"> <Image className="dashboard-icon" src="/images/dashboard/settings.png" alt="" />Impostazioni</li>
+            <li className="dashboard-li"> <Image className="dashboard-icon" src="/images/dashboard/user.png" alt="" />Il mio Account</li>
         </ul>
     </div>
  )
