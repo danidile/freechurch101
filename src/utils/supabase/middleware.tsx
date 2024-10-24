f5b48108698151681c252ae590588148abc75f80
Daniele Di Lecce <alessialauriero@Dani-Mac.local>
2024-10-25 00:01:21 +0200

    commit to check lsast time before aws

--- src/utils/supabase/middleware.tsx
+++ src/utils/supabase/middleware.tsx
@@ -1,5 +1,6 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
+import { secret } from '@aws-amplify/backend';
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
@@ -8,8 +9,8 @@ export async function updateSession(request: NextRequest) {
 
 
   const supabase = createServerClient( 
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    secret('NEXT_PUBLIC_SUPABASE_URL'),
+    secret('NEXT_PUBLIC_SUPABASE_ANON_KEY'),
     {
       cookies: {
         getAll() {
