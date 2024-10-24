15f8844f55cf3b450b8e7b5c40aad6d8c03e7506
Daniele Di Lecce <alessialauriero@Dani-Mac.local>
2024-10-25 00:01:21 +0200

    commit to check lsast time before aws

--- src/utils/supabase/server.tsx
+++ src/utils/supabase/server.tsx
@@ -1,12 +1,13 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
+import { secret } from '@aws-amplify/backend';
 
 export function createClient() {
   const cookieStore = cookies()
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    secret('NEXT_PUBLIC_SUPABASE_URL'),
+    secret('NEXT_PUBLIC_SUPABASE_ANON_KEY'),
     {
       cookies: {
         getAll() {
