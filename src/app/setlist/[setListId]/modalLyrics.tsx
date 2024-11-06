d99adafe9668c1880c81c3c225f95973ba92e2a8
Daniele Di Lecce <alessialauriero@Dani-Mac.local>
2024-11-06 19:02:55 +0100

    updates general

--- src/app/setlist/[setListId]/modalLyrics.tsx
+++ src/app/setlist/[setListId]/modalLyrics.tsx
@@ -5,26 +5,28 @@ import { TsongSchema } from "@/utils/types/types";
 import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from "@nextui-org/react";
 import AudioFileIcon from '@mui/icons-material/AudioFile';
 import ChordProViewComponent from "@/app/components/chordProViewComponent";
-export default function ModalLyrics( {songData}:{songData:Array<TsongSchema>},{size}:{size: string} ) {
+export default function ModalLyrics( {songData}:{songData:Array<TsongSchema>}) {
   const {isOpen, onOpen, onOpenChange} = useDisclosure();
 console.log(songData);
 const { width }: any = useWindowSize();
+let size : string =  "full";
+
 if(width > 1100){
-    const size =  '4xl';
+     size =  "4xl";
 }else{
-    const size = 'full';
+     size = "full";
 }
   return (
     <>
       <button onClick={onOpen}><AudioFileIcon/></button>
-      
-      <Modal 
+      {
+        width> 1000 && <Modal 
         backdrop="opaque" 
         isOpen={isOpen} 
         onOpenChange={onOpenChange}
         scrollBehavior="inside"
         isDismissable={true}
-        size={String(size)}
+        size="3xl"
         classNames={{
             backdrop: "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20"
           }}
@@ -68,6 +70,61 @@ if(width > 1100){
           )}
         </ModalContent>
       </Modal>
+      } 
+      {
+        width< 1000 && <Modal 
+        backdrop="opaque" 
+        isOpen={isOpen} 
+        onOpenChange={onOpenChange}
+        scrollBehavior="inside"
+        isDismissable={true}
+        size="full"
+        classNames={{
+            backdrop: "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20"
+          }}
+        motionProps={{
+          variants: {
+            enter: {
+              y: 0,
+              opacity: 1,
+              transition: {
+                duration: 0.3,
+                ease: "easeOut",
+              },
+            },
+            exit: {
+              y: -20,
+              opacity: 0,
+              transition: {
+                duration: 0.2,
+                ease: "easeIn",
+              },
+            },
+          }
+        }}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1"></ModalHeader>
+              <ModalBody>
+                <ChordProViewComponent songData={songData}/>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+      } 
+      
+        
     </>
   );
 }
