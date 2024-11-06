8ed7500e5f400c1c02f7df387bfe8d16e9bc61c5
Daniele Di Lecce <alessialauriero@Dani-Mac.local>
2024-11-06 18:46:00 +0100

    updates general

--- src/utils/hooks/useWindowDimensions.tsx
+++ src/utils/hooks/useWindowDimensions.tsx
@@ -1,29 +1,31 @@
-"use client"
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 
-type WindowDimentions = {
-    width: number | undefined;
-    height: number | undefined;
-};
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
 
-const useWindowDimensions = (): WindowDimentions => {
-    const [windowDimensions, setWindowDimensions] = useState<WindowDimentions>({
-        width: undefined,
-        height: undefined,
-    });
-    useEffect(() => {
-        function handleResize(): void {
-            setWindowDimensions({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
-        }
-        handleResize();
-        window.addEventListener('resize', handleResize);
-        return (): void => window.removeEventListener('resize', handleResize);
-    }, []); // Empty array ensures that effect is only run on mount
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
 
-    return windowDimensions;
-};
+    // Add event listener
+    window.addEventListener("resize", handleResize);
 
-export default useWindowDimensions;
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+}
+
+export default useWindowSize;
