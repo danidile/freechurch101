ed41193dd928ad3d6c7a29bef1aaa60aaefdc06a
Daniele Di Lecce <alessialauriero@Dani-Mac.local>
2024-11-08 09:27:15 +0100

    updates general

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout({
         rel="stylesheet"
         href="https://fonts.googleapis.com/icon?family=Material+Icons"
       />
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
       </head>
    <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50`}
