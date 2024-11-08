8f5ba02704b7645f6f689c2ff584643aab5d191b
Daniele Di Lecce <alessialauriero@Dani-Mac.local>
2024-11-08 11:31:56 +0100

    updates general

--- src/app/setlist/[setListId]/page.tsx
+++ src/app/setlist/[setListId]/page.tsx
@@ -10,10 +10,20 @@ import MusicNoteIcon from '@mui/icons-material/MusicNote';
 export default async function Page({ params }: {params:{setListId: string}}) {
   const setlistData = await getSetList(params.setListId) ;
   const setlistsongs = await getSetListSongs(params.setListId);
+  const date = new Date(setlistData.date);
+        const readableDate = date.toLocaleString("it-IT", {
+          weekday: "long", // "Sunday"
+          year: "numeric", // "2024"
+          month: "long",   // "November"
+          day: "numeric",  // "10"
+          // hour: "2-digit", // "10"
+          // minute: "2-digit", // "22"
+          // second: "2-digit", // "46"
+        });
     return (
     <div>
       <h6><strong>{setlistData.church.church_name}</strong></h6>
-      <p>{setlistData.date}</p>
+      <p>{readableDate}</p>
       
       {setlistsongs.map((song, index) =>{
         const songData = [ song.songTitle , song.lyrics]
