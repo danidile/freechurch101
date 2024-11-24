284433097a333ca633d9bc76851992c24b1525d6
Daniele Di Lecce <danidile94@gmail.com>
2024-11-24 23:29:32 +0100

    General update
    General update

--- src/app/sundayPlanner/CreateSundayPlan/sundayPlanForm.tsx
+++ src/app/sundayPlanner/CreateSundayPlan/sundayPlanForm.tsx
@@ -1,7 +1,7 @@
 "use client";
-import {Select, SelectItem} from "@nextui-org/react";
+import { Autocomplete, AutocompleteItem, Select, SelectItem } from "@nextui-org/react";
 import { Button } from "@nextui-org/button";
-import { Input } from "@nextui-org/input";
+import { Input, Textarea } from "@nextui-org/input";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 // import {addEvent} from './addEventAction';
@@ -32,8 +32,9 @@ type formValues = {
   };
 };
 
+export default function SundayPlanForm({songsList}: {songsList : TsongNameAuthor[]}) {
+  const newSongList = songsList;
 
-export default function SundayPlanForm() {
   const teamMembers = [
     {
       id: "1",
@@ -107,7 +108,7 @@ export default function SundayPlanForm() {
       memberId: "1",
       label: "Daniele Di Lecce",
       role: "Cantante, Chitarrista",
-    }
+    },
   });
   const [eventIsOther, setEventIsOther] = useState(false);
   let x: string;
@@ -145,7 +146,7 @@ export default function SundayPlanForm() {
     watch,
     formState: { isSubmitting },
   } = useForm<formValues>({
-    defaultValues:{
+    defaultValues: {
       eventType: "0",
       eventTitle: "Culto domenicale",
       date: "",
@@ -154,11 +155,10 @@ export default function SundayPlanForm() {
         memberId: "1",
         label: "Daniele Di Lecce",
         role: "Cantante, Chitarrista",
-      }
-    }
+      },
+    },
   });
   const watchAllFields = watch(); // when pass nothing as argument, you are watching everything
-
 
   // section => section[1] === event.target.id)
   const removeSection = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -169,7 +169,7 @@ export default function SundayPlanForm() {
   };
   const istypeother = (event: React.ChangeEvent<HTMLSelectElement>) => {
     console.log(event.target.value);
-    
+
     setEventDetails({ ...eventDetails, eventType: event.target.value });
     if (event.target.value == "5") {
       setEventIsOther(true);
@@ -200,114 +200,56 @@ export default function SundayPlanForm() {
     // addSetlist(watchAllFields);
   };
 
-
   return (
     <>
-      <div className=" flex flex-row rounded-xl gap-3 ">
-        <div className="form-div crea-setlist-container">
           <form onSubmit={handleSubmit(convertData)}>
-            <h4>Crea Setlist</h4>
-            <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-              <div className="transpose-button-container">
-                <Button
-                  color="primary"
+            <h4>Pianifica Evento</h4>
+              <div className="event-info-header">
+                <Select
+                  {...register(`eventType`)}
+                  label="Tipo di evento"
                   variant="flat"
-                  type="button"
-                  id="Canzone"
-                  onClick={AddSection}
+                  size="sm"
+                  placeholder="Riunione dei Giovani..."
+                  onChange={istypeother}
                 >
-                  Aggiungi evento
-                </Button>
-                
-              </div>
-              <div>
-                <Accordion
-                  selectionBehavior="replace"
-                  isCompact={true}
-                  selectionMode="single"
-                  variant="light"
-                  className="gap-4"
-                  keepContentMounted={true}
-                >
-                  {state.map((element, index) => {
-                    return (
-                      <AccordionItem
-                        className="accordian-setlist"
-                        startContent={<ArticleIcon />}
-                        key={element.key}
-                        aria-label="Accordion 1"
-                        title="Evento"
-                      >
-                        <div className="accordian-container">
-                          <Select
-                            {...register(`eventType`)}
-                            label="Tipo di evento"
-                            size="sm"
-                            placeholder="Riunione dei Giovani..."
-                            onChange={istypeother}
-                          >
-                            {tipoEvento.map((evento: string, index) => (
-                              <SelectItem key={index} value={evento}>
-                                {evento}
-                              </SelectItem>
-                            ))}
-                          </Select>
-                          {eventIsOther && (
-                            <Input
-                            {...register(`eventTitle`)}
-                              type="text"
-                              label="Aggiungi Titolo evento"
-                              variant="bordered"
-                              size="sm"
-                              onChange={editTitle}
-                            />
-                          )}
+                  {tipoEvento.map((evento: string, index) => (
+                    <SelectItem key={index} value={evento}>
+                      {evento}
+                    </SelectItem>
+                  ))}
+                </Select>
+                {eventIsOther && (
+                  <Input
+                    {...register(`eventTitle`)}
+                    type="text"
+                    label="Aggiungi Titolo evento"
+                    variant="flat"
+                    size="sm"
+                    onChange={editTitle}
+                  />
+                )}
 
-                          <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-                            <Input
-                              type="date"
-                              {...register(`date`)}
-                              label="Event Date"
-                              variant="bordered"
-                              size="sm"
-                            />
-                          </div>
-                          <Input
-                            name={"type" + element.key}
-                            key={element.key}
-                            value={element.id}
-                            className="hide-input"
-                          />
-                            <Select
-                              {...register(`test`)}
-                              label="Favorite Animal"
-                              selectionMode="multiple"
-                              placeholder="Select an animal"
-                            >
-                              {teamMembers.map((animal) => (
-                                <SelectItem key={animal.id}>
-                                  {animal.label}
-                                </SelectItem>
-                              ))}
-                            </Select>
-                          <div className="right-">
-                            <Button
-                              size="sm"
-                              isIconOnly
-                              type="button"
-                              variant="bordered"
-                              id={element.key}
-                              onClick={removeSection}
-                              accessKey={String(index)}
-                            >
-                              <DeleteForeverOutlinedIcon />
-                            </Button>
-                          </div>
-                        </div>
-                      </AccordionItem>
-                    );
-                  })}
-                </Accordion>
+                <Input
+                  type="date"
+                  {...register(`date`)}
+                  label="Event Date"
+                  variant="flat"
+                  size="sm"
+                />
+
+                <Select
+                  {...register(`test`)}
+                  label="Favorite Animal"
+                  variant="flat"
+                  size="sm"
+                  selectionMode="multiple"
+                  placeholder="Select an animal"
+                >
+                  {teamMembers.map((animal) => (
+                    <SelectItem key={animal.id}>{animal.label}</SelectItem>
+                  ))}
+                </Select>
               </div>
 
               <br />
@@ -317,12 +259,97 @@ export default function SundayPlanForm() {
                 type="submit"
                 disabled={isSubmitting}
               >
-                Salva modifiche
+                Crea Evento
               </Button>
+
+
+
+              <div className="event-info-header">
+        
+        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+        <h4>Crea Setlist</h4>
+
+        <h6>Aggingi sezione</h6>
+          
+
+          <div  className="transpose-button-container">
+                <Button variant="flat" type="button" id="Canzone"  onClick={AddSection}>Canzone</Button>
+                <Button variant="flat" type="button" id="Appunti"  onClick={AddSection}>Appunti</Button>
+          </div>
+            <div >
+            <Accordion selectionBehavior="replace" isCompact={true} selectionMode="single" variant="light" className="gap-4" keepContentMounted={true}>
+
+            {   
+                state.map((element,index) =>{
+                    
+                      return (
+                        <AccordionItem 
+                        className="accordian-setlist"
+                        startContent={<ArticleIcon/>}
+                        key={element.key} aria-label="Accordion 1" title={element.id}
+                        >
+
+                            <Input  
+                            name={"type"+element.key} key={element.key} value={element.id} className='hide-input' />
+
+                            
+                                        
+                            {element.isSong && (
+                                <Autocomplete 
+                                    size="sm"
+                                    fullWidth={true}
+                                    label="Seleziona la canzone" 
+                                    className="max-w-lg autocomplete-mobile-input"
+                                    disableAnimation={false}
+                                    
+                                >
+                                    {newSongList.map((song: Tsong, index) => {
+                                      
+                                        return (
+                                        
+                                    <AutocompleteItem key={song.id} title={song.song_title} description={song.author} textValue={song.song_title + " " + song.author + " #" + index }>
+
+                                    
+                                    </AutocompleteItem>
+                                    )})}
+                                </Autocomplete>                              
+                            )}
+                            <Textarea
+                                    className="my-2"
+                                    label="Descrizione"
+                                    
+                                    id={element.key}
+                                    size="sm"
+                                    labelPlacement="inside"
+                                    placeholder="Inserisci informazioni utili..."
+
+                                />
+                            <Button size="sm"
+                              className=" my-2"
+                              isIconOnly type='button'
+                              variant="bordered"
+                              id={element.key}
+                              onClick={removeSection}
+                              accessKey={String(index)}
+                            >
+                                <DeleteForeverOutlinedIcon/>
+                            </Button>   
+                            </AccordionItem>
+
+                            )
+                })
+            }
+            </Accordion>
+
             </div>
-          </form>
+
+          <br/>
+          <Button color="primary" variant="shadow" type='submit' disabled={isSubmitting}>
+          Add Song
+          </Button>
         </div>
-      </div>
+              </div>
+          </form>
     </>
   );
 }
