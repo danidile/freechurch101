01509afb169926fa720d0f82b25a33617d68de60
Daniele Di Lecce <danidile94@gmail.com>
2024-12-13 12:29:19 +0100

    make it accessible

--- src/app/songs/page.tsx
+++ src/app/songs/page.tsx
@@ -1,15 +1,40 @@
 import { createClient } from "@/utils/supabase/server";
 import SongslistComponent from "./songslistComponent";
 
+
+
+
+type songsListType = {
+  songs:{
+    id: string;
+    created_at: string;
+    song_title: string;
+    lyrics: string;
+    author: string;
+    upload_key: string;
+  }[];
+};
+
+
+
+
+
 export default async function Page() {
   const supabase = createClient();
-  const { data: songs } = await supabase.from("songs").select("*");
+  const { data: songs,error } = await supabase.from("songs").select("*");
+  console.log(songs);
+  if (error) {
+    console.error("Errore durante il fetch:", error);
+  }
+
+  const songList: songsListType = { songs: songs || [] }; // Avvolgi l'array in un oggetto
+
 
   if (songs) {
     return (
-      <>
-        <SongslistComponent songs={songs} />
-      </>
+      <div className="container-sub">
+        <SongslistComponent songs={songList.songs} />
+      </div>
     );
   } else {
     return (
