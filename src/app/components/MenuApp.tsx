150fe9fccc32309f9ca14b1422423a14df6d7ac8
Daniele Di Lecce <danidile94@gmail.com>
2025-01-27 18:54:39 +0100

    added bottom menu

--- src/app/components/MenuApp.tsx
+++ src/app/components/MenuApp.tsx
@@ -1,22 +1,20 @@
 "use client";
-
 import { Link } from "@nextui-org/react";
-
 import { IconContext } from "react-icons";
-
 import { HiUser } from "react-icons/hi2";
 import { FaMusic } from "react-icons/fa";
 import { MdEventNote } from "react-icons/md";
 import { FaHouse } from "react-icons/fa6";
 
 export default function MenuApp() {
+
   return (
     <div className="appmenucontainer">
       <IconContext.Provider
         value={{ size: "1.6rem", className: "app-menu-icons" }}
       >
         <div className="app-menu">
-          <Link href="/">
+          <Link href="/" >
             <FaHouse />
           </Link>
           <Link href="/songs">
