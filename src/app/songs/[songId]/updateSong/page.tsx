f9509c4073f75485624fd3ae0d3cad1b55a5d6b1
Daniele Di Lecce <danidile94@gmail.com>
2025-01-30 23:14:58 +0100

    created two separate lists for the songs, a global with all the italian artists and then I'll have to create a list for the church individual ones.

--- src/app/songs/[songId]/updateSong/page.tsx
+++ src/app/songs/[songId]/updateSong/page.tsx
@@ -1,13 +1,13 @@
 import { basicUserData } from "@/utils/types/userData";
-import { getSong } from "../getSong";
 import UpdateSongForm from "./updateSongForm";
 import { TsongSchema } from "@/utils/types/types";
 import fbasicUserData from "@/utils/supabase/getUserData";
 import { redirect } from "next/navigation";
+import { getSongById } from "@/hooks/GET/getSongById";
 
 
 export default async function Page({ params }: {params:{songId: string}}) {
-    const songData: TsongSchema = await getSong(params.songId);
+    const songData: TsongSchema = await getSongById(params.songId);
       const userData: basicUserData = await fbasicUserData();
     
     if(userData.role == "1" || userData.role == "2"){
