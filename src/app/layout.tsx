750342a5769a9398fad76a3e388401f85c4db198
Daniele Di Lecce <danidile94@gmail.com>
2025-02-06 17:09:19 +0100

    Added PNW splash images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -207,6 +207,50 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Add DM SANS font */}
+
+
+        {/* PWA Icons Splash */}
+        <link href="/images/brand/PWA-icons/apple-icon-180.png" rel="apple-touch-icon" sizes="180x180">
+<link href="/images/brand/PWA-icons/apple-splash-2048-2732.png" rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+<link href="/images/brand/PWA-icons/apple-splash-2732-2048.jpg" rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)">
+<link href="/images/brand/PWA-icons/apple-splash-1668-2388.jpg" rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+<link href="/images/brand/PWA-icons/apple-splash-2388-1668.jpg" rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)">
+<link href="/images/brand/PWA-icons/apple-splash-1536-2048.jpg" rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+<link href="/images/brand/PWA-icons/apple-splash-2048-1536.jpg" rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)">
+<link href="/images/brand/PWA-icons/apple-splash-1668-2224.jpg" rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+<link href="/images/brand/PWA-icons/apple-splash-2224-1668.jpg" rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)">
+<link href="/images/brand/PWA-icons/apple-splash-1620-2160.jpg" rel="apple-touch-startup-image" media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+<link href="/images/brand/PWA-icons/apple-splash-2160-1620.jpg" rel="apple-touch-startup-image" media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)">
+<link href="/images/brand/PWA-icons/apple-splash-1284-2778.jpg" rel="apple-touch-startup-image" media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+<link href="/images/brand/PWA-icons/apple-splash-2778-1284.jpg" rel="apple-touch-startup-image" media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)">
+<link href="/images/brand/PWA-icons/apple-splash-1170-2532.jpg" rel="apple-touch-startup-image" media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+<link href="/images/brand/PWA-icons/apple-splash-2532-1170.jpg" rel="apple-touch-startup-image" media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)">
+<link href="/images/brand/PWA-icons/apple-splash-1125-2436.jpg" rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+<link href="/images/brand/PWA-icons/apple-splash-2436-1125.jpg" rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)">
+<link href="/images/brand/PWA-icons/apple-splash-1242-2688.jpg" rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+<link href="/images/brand/PWA-icons/apple-splash-2688-1242.jpg" rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)">
+<link href="/images/brand/PWA-icons/apple-splash-828-1792.jpg" rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+<link href="/images/brand/PWA-icons/apple-splash-1792-828.jpg" rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)">
+<link href="/images/brand/PWA-icons/apple-splash-1242-2208.jpg" rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+<link href="/images/brand/PWA-icons/apple-splash-2208-1242.jpg" rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)">
+<link href="/images/brand/PWA-icons/apple-splash-750-1334.jpg" rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+<link href="/images/brand/PWA-icons/apple-splash-1334-750.jpg" rel="apple-touch-startup-image" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)">
+<link href="/images/brand/PWA-icons/apple-splash-640-1136.jpg" rel="apple-touch-startup-image" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+<link href="/images/brand/PWA-icons/apple-splash-1136-640.jpg" rel="apple-touch-startup-image" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)">
+
+
+
+        {/* END of PWA Icons Splash */}
+
+
+
+
+
+
+
+
+
+
         <link rel="icon" href="/images/brand/cropped-Fav-2.png" sizes="48x48" />
         <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/images/brand/180x180.png"></link>
