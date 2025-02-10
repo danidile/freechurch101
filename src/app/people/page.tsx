4e731b19fb571e491fc2c3fe5a1e3f0e82426902
Daniele Di Lecce <danidile94@gmail.com>
2025-02-10 15:08:23 +0100

    people work

--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 import fbasicUserData from "@/utils/supabase/getUserData";
 import { basicUserData } from "@/utils/types/userData";
-import { profileT, Team } from "@/utils/types/types";
+import { profileT } from "@/utils/types/types";
 import { getProfilesById } from "@/hooks/GET/getProfilesById";
 import PeopleDrawerList from "./peopleDrawerList";
 
@@ -9,20 +9,15 @@ export default async function App() {
   const userData: basicUserData = await fbasicUserData();
   const profiles: profileT[] = await getProfilesById(userData.church_id);
 
-  console.log("profiles");
-  console.log(profiles);
-
   if (userData) {
     return (
       <div className="container-sub ">
         <h3 className="pb-6">People</h3>
         <div className="flex-col gap-3">
-         {profiles.map((profile: profileT) => {
-            return (
-              <PeopleDrawerList profile={profile} key={profile.id} />
-            );
+          { profiles && profiles.map((profile: profileT) => {
+            return <PeopleDrawerList profile={profile} key={profile.id} />;
           })}
-          </div>
+        </div>
       </div>
     );
   } else {
