c9f1b7d525d865d358416690b94074126b763913
Daniele Di Lecce <danidile94@gmail.com>
2025-02-10 17:15:13 +0100

    people work

--- src/app/setlist/page.tsx
+++ src/app/setlist/page.tsx
@@ -1,80 +1,77 @@
-// @ts-nocheck
 import { basicUserData } from "@/utils/types/userData";
 import fbasicUserData from "../../utils/supabase/getUserData";
 
 import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
 import ListAltIcon from "@mui/icons-material/ListAlt";
-import { Reorder } from "framer-motion";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
-  const supabase = createClient();
-  const { data: setlist } = await supabase
-    .from("setlist")
-    .select('id, church("church_name"),event_title,date');
-
-  const currentDate = new Date();
-  const nextDate = new Date(currentDate)
-nextDate.setDate(currentDate.getDate() - 1)
-  const readableCurrentDate = currentDate.toLocaleString("it-IT", {
-    weekday: "long", // "Sunday"
-    year: "numeric", // "2024"
-    month: "long", // "November"
-    day: "numeric", // "10"
-    // hour: "2-digit", // "10"
-    // minute: "2-digit", // "22"
-    // second: "2-digit", // "46"
-  });
   const userData: basicUserData = await fbasicUserData();
-  console.log(userData);
-  if (setlist) {
-    return (
-      <div className="container-sub">
-       
+  if (userData) {
+    const supabase = createClient();
+    const { data: setlist } = await supabase
+      .from("setlist")
+      .select('id, church("church_name"),event_title,date')
+      .eq("church", userData.church_id);
 
-        <h5 className="text-center m-5">Lista eventi</h5>
-        {setlist.map((setlist) => {
-          const date = new Date(setlist.date);
-          const readableDate = date.toLocaleString("it-IT", {
-            weekday: "long", // "Sunday"
-            year: "numeric", // "2024"
-            month: "long", // "November"
-            day: "numeric", // "10"
-            // hour: "2-digit", // "10"
-            // minute: "2-digit", // "22"
-            // second: "2-digit", // "46"
-          });
-          if (nextDate <= date) {
-            return (
-              <div className="song-list" key={setlist.id}>
-                <Link
-                  className="song-list-link"
-                  href={`/setlist/${setlist.id}`}
-                >
-                  <p key={setlist.id}>
-                    {setlist.event_title}
-                    <br />
-                    <small>{readableDate}</small>{" "}
-                  </p>
-                  <ListAltIcon />
-                </Link>
-              </div>
-            );
-          }
-        })}
-         {["1", "2"].includes(userData.role.toString()) &&  (
-          <button className="button-transpose my-10">
-            <a href="/setlist/addSetlist">Crea nuova Setlist!</a>
-          </button>
-        )}
-      </div>
-    );
+    const currentDate = new Date();
+    const nextDate = new Date(currentDate);
+    nextDate.setDate(currentDate.getDate() - 1);
+    const readableCurrentDate = currentDate.toLocaleString("it-IT", {
+      weekday: "long", // "Sunday"
+      year: "numeric", // "2024"
+      month: "long", // "November"
+      day: "numeric", // "10"
+      // hour: "2-digit", // "10"
+      // minute: "2-digit", // "22"
+      // second: "2-digit", // "46"
+    });
+    console.log(userData);
+    if (setlist) {
+      return (
+        <div className="container-sub">
+          <h5 className="text-center m-5">Lista eventi</h5>
+          {setlist.map((setlist) => {
+            const date = new Date(setlist.date);
+            const readableDate = date.toLocaleString("it-IT", {
+              weekday: "long", // "Sunday"
+              year: "numeric", // "2024"
+              month: "long", // "November"
+              day: "numeric", // "10"
+              // hour: "2-digit", // "10"
+              // minute: "2-digit", // "22"
+              // second: "2-digit", // "46"
+            });
+            if (nextDate <= date) {
+              return (
+                <div className="song-list" key={setlist.id}>
+                  <Link
+                    className="song-list-link"
+                    href={`/setlist/${setlist.id}`}
+                  >
+                    <p key={setlist.id}>
+                      {setlist.event_title}
+                      <br />
+                      <small>{readableDate}</small>{" "}
+                    </p>
+                    <ListAltIcon />
+                  </Link>
+                </div>
+              );
+            }
+          })}
+          {["1", "2"].includes(userData.role.toString()) && (
+            <button className="button-transpose my-10">
+              <a href="/setlist/addSetlist">Crea nuova Setlist!</a>
+            </button>
+          )}
+        </div>
+      );
+    } else {
+      return redirect("/");
+    }
   } else {
-    return (
-      <>
-        <h1>No song found</h1>
-        <a href="/songs/addSong">Add a New Song!</a>
-      </>
-    );
+    return redirect("/");
   }
 }
