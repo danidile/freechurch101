6f0a9aac4187770e4df88770d08dd2eac2aeae70
Daniele Di Lecce <danidile94@gmail.com>
2025-02-19 09:55:23 +0100

    treams

--- src/app/setlist/[setListId]/update/page.tsx
+++ src/app/setlist/[setListId]/update/page.tsx
@@ -1,11 +1,12 @@
 import { getSetList } from "@/hooks/GET/getSetList";
 import UpdateSetlistForm from "./UpdateSetlistForm";
-import { setListSongT, setListT } from "@/utils/types/types";
+import { churchMembersT, setListSongT, setListT } from "@/utils/types/types";
 import { getSongsCompact } from "@/hooks/GET/getSongsCompact";
 import { getSetListSongsCompact } from "@/hooks/GET/getSetListSongsCompact";
 import { getChurchMembersCompact } from "@/hooks/GET/getChurchMembersCompact";
 import fbasicUserData from "@/utils/supabase/getUserData";
 import { basicUserData } from "@/utils/types/userData";
+import { getChurchWorshipTeam } from "@/hooks/GET/getChurchWorshipTeam";
 export default async function songs({
   params,
 }: {
@@ -16,7 +17,9 @@ export default async function songs({
     params.setListId
   );
   const userData: basicUserData = await fbasicUserData();
-  const churchMembers = await getChurchMembersCompact(userData.church_id);
+  const worshipTeamMembers: churchMembersT[] = await getChurchWorshipTeam(
+    userData.church_id
+  );
 
   setlistData.setListSongs = setlistsongs;
   const songs = await getSongsCompact();
@@ -26,7 +29,7 @@ export default async function songs({
         page="update"
         setlistData={setlistData}
         songsList={songs}
-        worshipTeamMembers={churchMembers}
+        worshipTeamMembers={worshipTeamMembers}
       />
     </div>
   );
