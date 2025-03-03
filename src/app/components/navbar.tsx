2421a9dc1199f924c3ad836526795d678431c691
Daniele Di Lecce <danidile94@gmail.com>
2025-03-03 12:28:09 +0100

    setlist teams

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,9 +1,12 @@
 import MenuBarComponent from "./menuBarComponent";
 import { basicUserData } from "@/utils/types/userData";
 import fbasicUserData from "../../utils/supabase/getUserData";
+import { notificationT } from "@/utils/types/types";
+import { getNotificationsById } from "@/hooks/GET/getNotificationsById";
 
 export default async function MenuBar() {
   const userData: basicUserData = await fbasicUserData();
-  
-  return <MenuBarComponent userData={userData} />;
+    const notifications: notificationT[] = await getNotificationsById(
+      userData.id);
+  return <MenuBarComponent notifications={notifications} userData={userData} />;
 }
