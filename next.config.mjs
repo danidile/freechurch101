8180a530912883da6f0e8779f583d0216c3eaa9a
Daniele Di Lecce <danidile94@gmail.com>
2025-03-06 10:06:24 +0100

    Resolved merge conflicts

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = {
+const nextConfig = {
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
 };
+
 export default nextConfig;
