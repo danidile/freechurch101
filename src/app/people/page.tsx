a267d8ba51599ca781d9fe1b527bc402dcd2f13a
Daniele Di Lecce <danidile94@gmail.com>
2025-05-05 22:20:57 +0200

    fixed some ui bugs and made it easyer also added donfirmation

--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -5,7 +5,9 @@ import { profileT } from "@/utils/types/types";
 import { getProfilesByChurch } from "@/hooks/GET/getProfilesByChurch";
 import PeopleDrawerList from "./peopleDrawerList";
 import GetParamsMessage from "../components/getParams";
-
+import Link from "next/link";
+import { TiUser } from "react-icons/ti";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 export default async function App() {
   const userData: basicUserData = await fbasicUserData();
@@ -21,11 +23,26 @@ export default async function App() {
           {profiles &&
             profiles.map((profile: profileT) => {
               return (
-                <PeopleDrawerList
-                  userData={userData}
-                  profile={profile}
-                  key={profile.id}
-                />
+                <div className="flex flex-row w-full gap-12" key={profile.id}>
+                  <Link
+                    className="people-link"
+                    href={`/people/${profile.id}`}
+                    key={profile.id}
+                  >
+                    <div className="people-list" key={profile.id}>
+                      <div className="flex flex-row gap-2 items-center">
+                        <TiUser color={profile.isTemp ? "#f5a524" : "black"} />
+                        <p key={profile.id}>
+                          {profile.name} {profile.lastname}
+                        </p>
+                      </div>
+
+                      <span className="material-symbols-outlined">
+                        <MoreVertIcon className="text-default-400" />
+                      </span>
+                    </div>
+                  </Link>
+                </div>
               );
             })}
         </div>
