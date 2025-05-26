72a60e37c458798adcc0aed8c9cf6aac36e6ce9b
Daniele Di Lecce <danidile94@gmail.com>
2025-05-26 18:44:24 +0100

    update

--- src/hooks/GET/getSongs.tsx
+++ src/hooks/GET/getSongs.tsx
@@ -7,14 +7,24 @@ import { basicUserData } from "@/utils/types/userData";
 export const getSongs = async () => {
   const supabase = createClient();
   const user: basicUserData = await fbasicUserData();
-  const { data: songs, error } = await supabase
-    .from("songs")
-    .select("*")
-    .order("song_title", { ascending: true })
-    .eq("church", user.church_id);
-  if (error) {
-    console.error("Errore durante il fetch:", error);
+  if (user.loggedIn) {
+    const { data: songs, error } = await supabase
+      .from("songs")
+      .select("*")
+      .order("song_title", { ascending: true })
+      .eq("church", user.church_id);
+    if (error) {
+      console.error("Errore durante il fetch:", error);
+    }
+    return songs;
+  } else {
+    const { data: songs, error } = await supabase
+      .from("global-songs")
+      .select("*")
+      .order("song_title", { ascending: true });
+    if (error) {
+      console.error("Errore durante il fetch:", error);
+    }
+    return songs;
   }
-
-  return songs;
 };
