fc0c5477dc9d1b34f4eb76b5a4bb831a1d775a89
Daniele Di Lecce <danidile94@gmail.com>
2025-06-03 19:10:23 +0200

    He

--- src/app/protected/dashboard/PWADashboard.tsx
+++ src/app/protected/dashboard/PWADashboard.tsx
@@ -15,6 +15,7 @@ import { TfiSharethis } from "react-icons/tfi";
 import { PiMusicNotesPlusFill } from "react-icons/pi";
 import { LuCalendarDays } from "react-icons/lu";
 import logoutAction from "@/app/components/logOutAction";
+import { useState } from "react";
 
 export default function PWADashboard({
   userData,
@@ -29,8 +30,26 @@ export default function PWADashboard({
   async function logouter() {
     logoutAction();
   }
+  const [avatarUrl, setAvatarUrl] = useState(
+    `https://kadorwmjhklzakafowpu.supabase.co/storage/v1/object/public/avatars/${userData.id}/avatar.jpg`
+  );
   return (
     <ul className="container-dashboard-list">
+      <div className="flex gap-6 justify-center items-center mb-4">
+        <img
+          className="avatar-pwa"
+          src={avatarUrl}
+          onError={() => {
+            setAvatarUrl("/images/userAvatarDefault.jpg"); // your default image path
+          }}
+        />
+        <div>
+          <h6 className="text-md font-bold">
+            {userData.name + " " + userData.lastname}
+          </h6>
+          <p>{userData.email}</p>
+        </div>
+      </div>
       {userData.pending_church_confirmation && (
         <Link
           className="dashboard-list !p-0"
@@ -82,10 +101,7 @@ export default function PWADashboard({
             <HiUserGroup className="dashboard-icon" />
             Teams
           </Link>
-          <Link
-            className="dashboard-list text-black"
-            href="/globalsongs"
-          >
+          <Link className="dashboard-list text-black" href="/globalsongs">
             <MdLibraryMusic className="dashboard-icon" />
             Global Songs
           </Link>
