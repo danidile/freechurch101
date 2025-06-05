7ac0a052ca860098ffbf0a6adecf6aa0bc3b51e2
Daniele Di Lecce <danidile94@gmail.com>
2025-06-05 15:06:56 +0200

    He

--- src/app/setlist/[setListId]/MoreDropdownSetlist.tsx
+++ src/app/setlist/[setListId]/MoreDropdownSetlist.tsx
@@ -4,7 +4,14 @@ import { MdDelete } from "react-icons/md";
 import { FaRegCopy } from "react-icons/fa";
 import { MdModeEdit } from "react-icons/md";
 import { RiSettings4Fill } from "react-icons/ri";
-
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@heroui/react";
 import {
   Dropdown,
   DropdownTrigger,
@@ -22,6 +29,8 @@ export default function MoreDropdowSetlist({
 }: {
   setlistId: string;
 }) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   const [copied, setCopied] = useState(false);
 
   const handleCopy = async () => {
@@ -37,38 +46,69 @@ export default function MoreDropdowSetlist({
     deleteSetList(setlistId);
   };
   return (
-    <Dropdown>
-      <DropdownTrigger>
-        <Button variant="flat" isIconOnly>
-          <RiSettings4Fill size={25} color="#000000" />
-        </Button>
-      </DropdownTrigger>
-      <DropdownMenu aria-label="Dropdown menu with shortcut" variant="flat">
-        <DropdownItem
-          startContent={<MdModeEdit />}
-          key="new"
-          className="text-center"
-        >
-          <Link
-            color="foreground"
-            className="w-full text-center"
-            size="sm"
-            href={`/setlist/${setlistId}/update`}
+    <>
+      <Dropdown>
+        <DropdownTrigger>
+          <Button variant="flat" isIconOnly>
+            <RiSettings4Fill size={25} color="#000000" />
+          </Button>
+        </DropdownTrigger>
+        <DropdownMenu aria-label="Dropdown menu with shortcut" variant="flat">
+          <DropdownItem
+            startContent={<MdModeEdit />}
+            key="new"
+            className="text-center"
           >
-            Aggiorna
-          </Link>
-        </DropdownItem>
-        <DropdownItem
-          startContent={<MdDelete />}
-          variant="flat"
-          onPress={deleteSetlist}
-          key="delete"
-          className="text-danger"
-          color="danger"
-        >
-          Elimina
-        </DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
+            <Link
+              color="foreground"
+              className="w-full text-center"
+              size="sm"
+              href={`/setlist/${setlistId}/update`}
+            >
+              Aggiorna
+            </Link>
+          </DropdownItem>
+          <DropdownItem
+            startContent={<MdDelete />}
+            variant="flat"
+            onPress={onOpen}
+            key="delete"
+            className="text-danger"
+            color="danger"
+          >
+            Elimina
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Elimina Evento
+              </ModalHeader>
+              <ModalBody>
+                <p>
+                  <span className="underline">
+                    Sei sicuro di voler eliminare questo evento?
+                  </span> {" "}
+                  Eliminerai tutti i dati relativi a questo evento. Se sì clicca
+                  su
+                  <strong>"Elimina"</strong> altrimenti clicca su cancella.
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button fullWidth color="primary" onPress={onClose}>
+                  Cancella
+                </Button>
+                <Button fullWidth color="danger" onPress={deleteSetlist}>
+                  Elimina
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
