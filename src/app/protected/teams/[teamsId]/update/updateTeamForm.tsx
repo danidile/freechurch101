c9520678165b3d258432028dc43ef0ca1354a7d6
Daniele Di Lecce <danidile94@gmail.com>
2025-06-09 18:06:19 +0200

    fix reset password

---
+++ src/app/protected/teams/[teamsId]/update/updateTeamForm.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { churchMembersT, teamData } from "@/utils/types/types";
+import { getChurchTeam } from "@/hooks/GET/getChurchTeam";
+import { getChurchMembersCompact } from "@/hooks/GET/getChurchMembersCompact";
+import TeamsForm from "../../teamsForm";
+import { useUserStore } from "@/store/useUserStore";
+import { useEffect, useState } from "react";
+import { Spinner } from "@heroui/spinner";
+
+export default function UpdateTeamForm({
+  params,
+}: {
+  params: { teamsId: string };
+}) {
+  const { userData, loading } = useUserStore();
+  const [churchMembers, setChurchMembers] = useState<churchMembersT[] | null>(
+    []
+  );
+  const [churchTeam, setChurchTeam] = useState<teamData | null>();
+  const [loadingSongs, setLoadingSongs] = useState(true);
+
+  useEffect(() => {
+    const fetchChurchMembers = async () => {
+      if (!loading && userData.loggedIn) {
+        const fetchedSongs = await getChurchMembersCompact(userData.church_id);
+        setChurchMembers(fetchedSongs);
+        const fetchedTeam = await getChurchTeam(params.teamsId);
+        setChurchTeam(fetchedTeam);
+        setLoadingSongs(false);
+      }
+    };
+
+    fetchChurchMembers();
+  }, [loading, userData.loggedIn]);
+  if (loadingSongs || !churchMembers) {
+    return (
+      <div className="container-sub">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+  return (
+    <div className="container-sub">
+      <TeamsForm
+        page="update"
+        churchTeam={churchTeam}
+        churchMembers={churchMembers}
+      />
+    </div>
+  );
+}
