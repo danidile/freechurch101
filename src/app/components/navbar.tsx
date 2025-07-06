af4a8f0b5d75537663376788e73752bb0da05758
Daniele Di Lecce <danidile94@gmail.com>
2025-07-06 20:53:51 +0200

    fixed reset password

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,10 +2,6 @@
 
 import MenuBarComponentSecondary from "./menuBarComponent2";
 
-import { useUserStore } from "@/store/useUserStore";
-
 export default function MenuBar() {
-  const { userData } = useUserStore();
-
   return <MenuBarComponentSecondary />;
 }
