79edcebf939b3b1453b7b5a197e55da8e2ba59e0
Daniele Di Lecce <danidile94@gmail.com>
2025-07-06 20:37:41 +0200

    fixed reset password

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,22 +1,11 @@
 "use client";
 
 import MenuBarComponentSecondary from "./menuBarComponent2";
-import { useEffect, useState } from "react";
-import { getPendingNotificationsById } from "@/hooks/GET/getPendingNotificationsById";
+
 import { useUserStore } from "@/store/useUserStore";
 
 export default function MenuBar() {
-  const [notifications, setNotifications] = useState<number>(0);
   const { userData } = useUserStore();
 
-  useEffect(() => {
-    const fetchNotifications = async () => {
-      if (userData.fetched && userData.loggedIn) {
-        const count = await getPendingNotificationsById();
-        setNotifications(count);
-      }
-    };
-    fetchNotifications();
-  }, []);
-  return <MenuBarComponentSecondary notifications={notifications} />;
+  return <MenuBarComponentSecondary />;
 }
