8c2f96bd53a8b5a60a757e411eb6f4660e797662
Daniele Di Lecce <danidile94@gmail.com>
2025-07-16 10:26:21 +0200

    vfe

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"], // add ts/jsx/tsx if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
