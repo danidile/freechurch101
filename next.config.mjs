d19a623999a5cded9ee73b8b27c55f156b80df16
Daniele Di Lecce <danidile94@gmail.com>
2025-08-15 20:34:15 +0200

    Added loggin for setlist update

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const isProd = process.env.NODE_ENV !== "development";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   async headers() {
