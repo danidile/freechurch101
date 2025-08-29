8ba2b4a049bc7fb59e38f6c33ac1bee168f80b5a
Daniele Di Lecce <danidile94@gmail.com>
2025-08-29 14:40:23 -0400

    fixed chords view mode

--- next.config.mjs
+++ next.config.mjs
@@ -28,6 +28,7 @@ font-src https://fonts.gstatic.com;
 img-src 'self' data: blob: https://kadorwmjhklzakafowpu.supabase.co https://*.stripe.com https://churchlab.it https://*.churchlab.it;
 connect-src 'self' https://kadorwmjhklzakafowpu.supabase.co https://api.stripe.com https://webhooks.stripe.com https://hooks.stripe.com https://js.stripe.com https://churchlab.it https://*.churchlab.it;
 frame-src https://js.stripe.com https://hooks.stripe.com;
+media-src 'self' https://kadorwmjhklzakafowpu.supabase.co;
 worker-src blob: 'self';
 `.replace(/\n/g, ""),
                 },
