7518a957d13c84f77b55ef5ed03331340d39febf
Daniele Di Lecce <danidile94@gmail.com>
2026-06-02 17:35:12 -0700

    fixed sidebar on mobile and links

--- src/app/[locale]/protected/dashboard/page.tsx
+++ src/app/[locale]/protected/dashboard/page.tsx
@@ -1,6 +1,7 @@
-import { redirect } from "@/i18n/navigation";
-export default function App({ params }: { params: { locale: string } }) {
-  redirect({ href: "/protected/dashboard/account", locale: params.locale });
+import Dashboard from "@/app/[locale]/protected/dashboard/dashboard-components/dashboard";
+import { redirect } from "next/navigation";
+export default function App() {
+  redirect("/it/protected/dashboard/account");
 
-  return <div></div>;
+  return <Dashboard />;
 }
