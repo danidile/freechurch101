262e6dded1209c84e96e690b872943fc548f7110
Daniele Di Lecce <danidile94@gmail.com>
2026-06-02 17:22:18 -0700

    fixed sidebar on mobile and links

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -5,7 +5,7 @@ export default function manifest(): MetadataRoute.Manifest {
     name: "Church Lab",
     short_name: "Church Lab",
     description: "A Progressive Web App built with Next.js",
-    start_url: "/it/protected/dashboard",
+    start_url: "/it/protected/dashboard/account",
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#ffffff",
