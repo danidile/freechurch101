a96f8bcd6e4087078951e495d14994065f2b67a0
Daniele Di Lecce <danidile94@gmail.com>
2026-06-27 22:20:42 -0700

    nuovo sistema di auth con auth_id e id

--- src/utils/supabase/middleware.tsx
+++ src/utils/supabase/middleware.tsx
@@ -3,7 +3,7 @@ import { NextResponse, type NextRequest } from "next/server";
 
 export async function updateSession(request: NextRequest) {
   const response = NextResponse.next();
-
+ 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
